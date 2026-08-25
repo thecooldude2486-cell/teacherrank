2fbeece1599f956e97850294ec1fe929d1b3c51d
thecooldude2486-cell <thecooldude2486@gmail.com>
2026-08-26 09:06:22 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
+
+    spa: {
+      enabled: true,
+      prerender: {
+        outputPath: "/index.html",
+      },
+    },
   },
 
   vite: {
